24eb722ab11a9cb5559c684e79bf3063e1ee17cc
Albert Mokrejs <albertmokrejs@gmail.com>
2015-10-18 15:35:08 -0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,25 +2,25 @@ import sqlite3, os.path
 
 x = os.path.isfile("StoryBase.db")
 
-connect = sqlite3.connect("StoryBase.db")
-curs = connect.cursor()
-
-List = ["""
-CREATE TABLE Login(
-   Username TEXT,
-   Password TEXT
-);""","""CREATE TABLE Stories(
-   Content TEXT, 
-   Name TEXT,
-   Username TEXT,
-   ID REAL,
-   Date TEXT
-);""","""CREATE TABLE comments(storyID REAL,
-   CContent TEXT,
-   Date TEXT
-);
-""","""CREATE TABLE StoryID(storyID REAL);
-"""]
-for q in List:
-   curs.execute(q)
-   connect.commit()
+if x:
+   connect = sqlite3.connect("StoryBase.db")
+   curs = connect.cursor()
+   List = ["""
+   CREATE TABLE Login(
+      Username TEXT,
+      Password TEXT
+   );""","""CREATE TABLE Stories(
+      Content TEXT, 
+      Name TEXT,
+      Username TEXT,
+      ID REAL,
+      Date TEXT
+   );""","""CREATE TABLE comments(storyID REAL,
+      CContent TEXT,
+      Date TEXT
+   );
+   ""","""CREATE TABLE StoryID(storyID REAL);
+   """]
+   for q in List:
+      curs.execute(q)
+      connect.commit()
