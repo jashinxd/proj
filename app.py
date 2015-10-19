d8c98112ac7ea417317a05b024f913681992d098
Albert Mokrejs <albertmokrejs@gmail.com>
2015-10-18 23:40:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,10 @@ def storypage():
     FROM Stories;
     """
     conn = sqlite3.connect("StoryBase.db")
+    xonn = sqlite3.connect("StoryBase.db")
     c = conn.cursor()
     result = c.execute(q)
+    x = xonn.cursor()
     MainHTML = ""
     for r in result:
     	StoryHTML = """ 
@@ -74,7 +76,7 @@ def storypage():
         SELECT *
     	FROM comments where storyID = %s;
     	""" % (r[3])
-    	comments = c.execute(d)
+    	comments = x.execute(d)
     	for y in comments:
     		StoryHTML += '<p style="font-size:70%">'
     		commentHTML = """
